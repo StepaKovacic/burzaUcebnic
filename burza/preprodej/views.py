d6cf798bd7899087ca2043f4b1468b1d4aed95d3
xXEpicWebDevXx <pyaholetestacc@gmail.com>
2023-10-22 21:37:39 +0200

    basic searchBar

--- burza/preprodej/views.py
+++ burza/preprodej/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
-from .models import Offer
+from .models import Offer, Book
+from .forms import SearchForm
+
 # Create your views here.
 def home(request):
+	emptyForm = SearchForm()
+	if request.method == "POST":
+		form = SearchForm(request.POST)
+		if form.is_valid():
+			books = Book.objects.filter(name__startswith = form.cleaned_data['search'])
+			offers = []
+			for book in books:
+				offers += Offer.objects.filter(book = book)
+
+		return render(request,'home.html',context={'offers':offers,'form':emptyForm})
 	offers = Offer.objects.all()
 
-
-	return render(request, "home.html", {'offers':offers})
+	
+	return render(request, "home.html", {'offers':offers,'form':emptyForm})
 
 
 def create(request):
