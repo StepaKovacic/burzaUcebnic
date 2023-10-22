ce64232fd3e9dace83deb8d811a2db50bf849a93
Štěpán Kovačič <stepan.kovacic@gmail.com>
2023-10-22 14:18:22 +0200

    Edit of models - indexing

--- burza/preprodej/models.py
+++ burza/preprodej/models.py
@@ -5,12 +5,18 @@ class Book(models.Model):
     name = models.CharField(max_length=200)
     img_url = models.CharField(max_length = 200)
 
+    def __str__(self):
+        return self.name
+
 
 class Offer(models.Model):
     description = models.CharField(max_length=500)
     state = models.IntegerField()   #Kolik z deseti třeba. Mby neni important
     contact_info = models.CharField(max_length=200)
     book = models.ForeignKey(Book,on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.description
 
 
 
