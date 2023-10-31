645bf9f86e755c5efca6c7a962f79465cee7339b
xXEpicWebDevXx <pyaholetestacc@gmail.com>
2023-10-31 21:03:24 +0100

    autocomplete half-done

---
+++ burza/preprodej/static/autoComplete.js
@@ -0,0 +1,24 @@
+const bookNames = [];
+listOfBooks.forEach((element) =>bookNames.push(element['name']))
+
+
+
+
+const searchBar = document.getElementById("searchBar")
+
+
+
+const results = []
+
+
+searchBar.onkeyup = function(){
+    let input = searchBar.value;
+    if (input.length){ // pokud input existuje => je tam nejaky text idk
+        forea
+    }
+}
+
+
+
+
+console.log(bookNames)
--- burza/preprodej/views.py
+++ burza/preprodej/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Offer, Book
 from .forms import SearchForm
+from json import dumps
 
 # Create your views here.
 def home(request):
@@ -16,11 +17,10 @@ def home(request):
 		return render(request,'home.html',context={'offers':offers,'form':emptyForm})
 	offers = Offer.objects.all()
 
-	
-	return render(request, "home.html", {'offers':offers,'form':emptyForm})
+	books = list(Book.objects.values())
+	return render(request, "home.html", {'offers':offers,'form':emptyForm, 'books':dumps(books)})
 
 
 def create(request):
 	if request.method == "POST":
 		pass
-
